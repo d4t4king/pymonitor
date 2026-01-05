c41768a16588840d88a2a663b215ba514c126f67
d4t4king <dataking@gmail.com>
2026-01-05 06:43:27 -0500

    sqlite handler wip

--- sys_poll.py
+++ sys_poll.py
@@ -20,11 +20,54 @@ import pprint
 import json
 import os
 import time
-import sys
+# import sys
 from typing import Dict, List, Any, Union
 
 import psutil
 import logging
+import sqlite3
+
+class SQLiteHandler():
+    CATEGORIES_INSERT_STMTS = {
+        "cpu": "INSERT INTO cpu_metrics (timestamp, cpu_percent, cpu_count_logical, cpu_count_physical) VALUES (?, ?, ?, ?)",
+        "memory": "INSERT INTO memory_metrics (timestamp, total, available, percent, used) VALUES (?, ?, ?, ?, ?)",
+        "swap": "INSERT INTO swap_metrics (timestamp, total, free, percent, used) VALUES (?, ?, ?, ?, ?)",
+        "disk": "INSERT INTO disk_metrics (timestamp, path, total, free, percent) VALUES (?, ?, ?, ?, ?)",
+        "net_if": "INSERT INTO net_if_metrics (timestamp, ifname, isup, mtu, speed_mbps, ips) VALUES (?, ?, ?, ?, ?, ?)",
+        "net_errors": "INSERT INTO net_errors_metrics (timestamp, ifname, errin, errout, dropin, dropout) VALUES (?, ?, ?, ?, ?, ?)",
+        "bandwidth": "INSERT INTO bandwidth_metrics (timestamp, ifname, sent_Bps, recv_Bps) VALUES (?, ?, ?, ?)",
+    }
+
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path)
+        self.initialize_tables()
+
+    def initialize_tables(self) -> None:
+        cursor = self.conn.cursor()
+        tables_to_initialize = {
+            "cpu": "CREATE TABLE IF NOT EXISTS cpu_metrics (timestamp TEXT, cpu_percent REAL, cpu_count_logical INTEGER, cpu_count_physical INTEGER)",
+            "memory": "CREATE TABLE IF NOT EXISTS memory_metrics (timestamp TEXT, total INTEGER, available INTEGER, percent REAL, used INTEGER)",
+            "swap": "CREATE TABLE IF NOT EXISTS swap_metrics (timestamp TEXT, total INTEGER, free INTEGER, percent REAL, used INTEGER)",
+            "disk": "CREATE TABLE IF NOT EXISTS disk_metrics (timestamp TEXT, path TEXT, total INTEGER, free INTEGER, percent REAL)",
+            "net_if": "CREATE TABLE IF NOT EXISTS net_if_metrics (timestamp TEXT, ifname TEXT, isup INTEGER, mtu INTEGER, speed_mbps INTEGER, ips TEXT)",
+            "net_errors": "CREATE TABLE IF NOT EXISTS net_errors_metrics (timestamp TEXT, ifname TEXT, errin INTEGER, errout INTEGER, dropin INTEGER, dropout INTEGER)",
+            "bandwidth": "CREATE TABLE IF NOT EXISTS bandwidth_metrics (timestamp TEXT, ifname TEXT, sent_Bps INTEGER, recv_Bps INTEGER)",
+        }
+        for table_name, create_stmt in tables_to_initialize.items():
+            cursor.execute(create_stmt)
+            self.conn.commit()
+
+    def insert_metrics(self, category: str, data: Dict[str, Any]) -> None:
+        cursor = self.conn.cursor()
+        timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='milliseconds').replace('+00:00', 'Z')
+        data_json = json.dumps(data)
+        cursor.execute(self.CATEGORIES_INSERT_STMTS[category], (timestamp, *data.values()))
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+        
 
 SCRIPT_NAME = os.path.basename(__file__)
 # Just checking if __file__ gives us the full path. (Which it does.)
@@ -153,11 +196,9 @@ def measure_bandwidth(interval: float = 1.0, ifname: str | None = None) -> Dict[
         out[name] = {"sent_Bps": int(sent_per_s), "recv_Bps": int(recv_per_s)}
     return out
 
-
 DEFAULT_CATEGORIES = ["cpu", "memory", "swap", "disk", "net_if", "net_errors"]
 
-
-def run_categories(categories: List[str], logfile: str ="syspoll.log", quiet: bool =False, include_loopback: bool =False) -> None:
+def run_categories(categories: List[str], sqlite: bool, logfile: str, quiet: bool =False, include_loopback: bool =False) -> None:
     # logging.basicConfig(filename=logfile)
     # Configure Logging
     logging.basicConfig(
@@ -208,12 +249,11 @@ def run_categories(categories: List[str], logfile: str ="syspoll.log", quiet: bo
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(description="Simple cron-friendly system poller")
     p.add_argument("categories", nargs="?", help="Comma-separated categories to collect")
-    p.add_argument(
-        '-q', '--quiet', 
-        dest='quiet',  
-        action='store_true', 
-        help="Cron mode.  Suppress all output except errors."
-    )
+    vqd = p.add_mutually_exclusive_group()
+    vqd.add_argument('-v', '--verbose', dest='verbose', action='store_false', help="Verbose mode.  Emit all output.")
+    vqd.add_argument('-q', '--quiet', dest='quiet', action='store_true', help="Cron mode.  Suppress all output except errors.")
+    vqd.add_argument('-d', '--debug', dest='debug', action='store_true', help="Debug mode.  Emit extra debug output.")
+    p.add_argument('-s', '-sqlite', dest='sqlite', action='store_true', help="Store output in SQLite database.")
     p.add_argument(
         "--include-loopback",
         action="store_true",
@@ -231,6 +271,10 @@ def main() -> None:
     pp = pprint.PrettyPrinter(indent=4)
 
     args = parse_args()
+    if args.sqlite and not args.logfile:
+        args.logfile = "syspoll.sqlite"
+    elif not args.logfile:
+        args.logfile = "syspoll.log"
 
     #pp.pprint(sys.version_info)
     #print(f"include_loopback is of type {str(type(args.include_loopback))} with value {args.include_loopback}")
@@ -239,7 +283,7 @@ def main() -> None:
         cats = [c.strip() for c in args.categories.split(",") if c.strip()]
     else:
         cats = DEFAULT_CATEGORIES
-    run_categories(cats, logfile=args.logfile, quiet=args.quiet, include_loopback=args.include_loopback)
+    run_categories(cats, args.sqlite, logfile=args.logfile, quiet=args.quiet, include_loopback=args.include_loopback)
 
 
 if __name__ == "__main__":
